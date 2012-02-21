9469df84a572ed2b848ca9dd37447d8649fa9ab6
Grant Mathews <grant@causes.com>
2012-02-20 18:37:10 -0800

    Getting there.

--- translate.py
+++ translate.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import markdown
 
 html = ""
@@ -8,33 +9,41 @@ footnotebuff = ""
 
 def process(text):
   result = markdown.markdown(text)
-  return result[3:-4]
+  return result
 
 def footnotify(item):
   global footnotebuff
   global footnotes
   match = item.group()
   num = match[match.find(" ") + 1 : -1]
-  footnotebuff += "<div class='footnote'>" + process(footnotes[num]) + "</div>"
-  return ""
+  footnotes[num] = "**" + num + ":** " + footnotes[num]
+  footnotebuff += process(footnotes[num]) + "\n"
+  return "<sup>" + num + "</sup>"
 
+# Gather all footnotes.
 for line in lines:
   line = line.strip()
   if line == "": continue
   if line.startswith("#"):
-    footnotes[line[1:line.find(" ")]] = line[line.find(" ") + 1:]
-    continue
+    footnote_num = line[1:line.find(" ")]
+    if footnote_num in footnotes:
+      footnotes[footnote_num] += line[line.find(" ") + 1:] + "\n\n"
+    else:
+      footnotes[footnote_num] = line[line.find(" ") + 1:] + "\n\n"
 
-  footnote = re.compile(r'#footnote (\d+)#')
+for line in lines:
+  line = line.strip()
+  if line == "": continue
+  if line.startswith("#"): continue
+
+  footnote = re.compile(r'#footnote ([^#]+)#')
   line = footnote.sub(footnotify, line)
-  if footnotebuff != "":
-    html += footnotebuff + "\n"
 
-  html += "<div class='normal'>" + process(line) + "</div>\n"
-
-  footnotebuff = ""
+  html += process(line) + "\n"
 
 header = "<link rel='stylesheet' type='text/css' href='style.css'> </link>\n<body>\n"
 footer = "</body>"
 
-print header + html + footer
+print header + "<div class='normal'>" + html + "</div> <div class='footnote'>" + footnotebuff + "</div>" + footer
+
+#sys.stderr.write(repr(footnotes))
